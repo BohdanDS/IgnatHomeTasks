ecfdf58cfdd28482289016205eb0feda4c996fdf
BohdanDS <bohdanpeliutkevich@gmail.com>
2022-03-01 20:02:36 +0300

    task 11

--- src/p2-homeworks/h11/common/c8-SuperDoubleRange/SuperDoubleRange.tsx
+++ src/p2-homeworks/h11/common/c8-SuperDoubleRange/SuperDoubleRange.tsx
@@ -1,46 +1,31 @@
-import React, {useEffect, useState} from 'react'
-import {ChangeEvent} from 'react'
+import React from 'react'
 import {Box, Slider} from "@mui/material";
 
 type SuperDoubleRangePropsType = {
     onChangeRange?: (value: [number, number]) => void
     value: [number, number]
-    // min?:number
-    // max?:number
-    // step?:number
-
-    // min, max, step, disable, ...
 }
 
 const SuperDoubleRange: React.FC<SuperDoubleRangePropsType> = (
     {
-        onChangeRange, value,
-         // min, max, step, ...props
+        onChangeRange, value
     }
 ) => {
     // сделать самому, можно подключать библиотеки
 
-    // console.log(onChangeRange)
-    // console.log(value)
-    // const handleChange = (event: Event, newValue: number | number[]) => {
-    //     onChangeRange && onChangeRange(newValue as [number, number]);
-    // };
-    // const changeHandler = (e:ChangeEvent<HTMLInputElement>) => {
-    //     setLocalValue(Number(e.currentTarget.value))
-    //     console.log(e.currentTarget.value)
-    // }
     const handleChange = (event: Event, newValue: number | number[]) => {
         console.log(newValue)
         onChangeRange && onChangeRange(newValue as [number, number]);
     };
     return (
         <>
-            <Box sx={{ width: 300, margin: "40px" }}>
+            <Box sx={{width: 300, margin: "40px"}}>
                 <Slider
                     getAriaLabel={() => 'Temperature range'}
                     value={value}
                     onChange={handleChange}
                     valueLabelDisplay="auto"
+                    color="secondary"
                 />
             </Box>
         </>
