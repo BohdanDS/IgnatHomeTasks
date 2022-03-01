12e4491caa3518fa61c431e709a907346914261e
BohdanDS <bohdanpeliutkevich@gmail.com>
2022-03-01 20:00:09 +0300

    task 11

--- src/p2-homeworks/h11/HW11.tsx
+++ src/p2-homeworks/h11/HW11.tsx
@@ -6,6 +6,12 @@ function HW11() {
     const [value1, setValue1] = useState(0)
     const [value2, setValue2] = useState(100)
 
+    const onChangeValue1 = (value: number) => setValue1(value)
+    const onChangeTwoValues = (value: [number, number]) => {
+        setValue1(value[0])
+        setValue2(value[1])
+    }
+
     return (
         <div>
             <hr/>
@@ -15,13 +21,17 @@ function HW11() {
             <div>
                 <span>{value1}</span>
                 <SuperRange
+                    onChangeRange={onChangeValue1}
+                    value={value1}
                     // сделать так чтоб value1 изменялось
                 />
             </div>
 
-            <div>
+            <div style={{display: "flex", height: '100px', justifyContent: "left", alignItems:"center"}}>
                 <span>{value1}</span>
                 <SuperDoubleRange
+                    value={[value1, value2]}
+                    onChangeRange={onChangeTwoValues}
                     // сделать так чтоб value1 и value2 изменялось
                 />
                 <span>{value2}</span>
--- src/p2-homeworks/h11/common/c8-SuperDoubleRange/SuperDoubleRange.tsx
+++ src/p2-homeworks/h11/common/c8-SuperDoubleRange/SuperDoubleRange.tsx
@@ -1,22 +1,48 @@
-import React from 'react'
+import React, {useEffect, useState} from 'react'
+import {ChangeEvent} from 'react'
+import {Box, Slider} from "@mui/material";
 
 type SuperDoubleRangePropsType = {
     onChangeRange?: (value: [number, number]) => void
-    value?: [number, number]
+    value: [number, number]
+    // min?:number
+    // max?:number
+    // step?:number
+
     // min, max, step, disable, ...
 }
 
 const SuperDoubleRange: React.FC<SuperDoubleRangePropsType> = (
     {
         onChangeRange, value,
-        // min, max, step, disable, ...
+         // min, max, step, ...props
     }
 ) => {
     // сделать самому, можно подключать библиотеки
 
+    // console.log(onChangeRange)
+    // console.log(value)
+    // const handleChange = (event: Event, newValue: number | number[]) => {
+    //     onChangeRange && onChangeRange(newValue as [number, number]);
+    // };
+    // const changeHandler = (e:ChangeEvent<HTMLInputElement>) => {
+    //     setLocalValue(Number(e.currentTarget.value))
+    //     console.log(e.currentTarget.value)
+    // }
+    const handleChange = (event: Event, newValue: number | number[]) => {
+        console.log(newValue)
+        onChangeRange && onChangeRange(newValue as [number, number]);
+    };
     return (
         <>
-            DoubleRange
+            <Box sx={{ width: 300, margin: "40px" }}>
+                <Slider
+                    getAriaLabel={() => 'Temperature range'}
+                    value={value}
+                    onChange={handleChange}
+                    valueLabelDisplay="auto"
+                />
+            </Box>
         </>
     )
 }
