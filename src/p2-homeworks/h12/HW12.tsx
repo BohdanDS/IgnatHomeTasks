66009390b7903c064a34c7e21706000e10fd4de7
BohdanDS <bohdanpeliutkevich@gmail.com>
2022-03-01 20:34:12 +0300

    task 12

--- src/p2-homeworks/h12/HW12.tsx
+++ src/p2-homeworks/h12/HW12.tsx
@@ -5,7 +5,7 @@ import {useDispatch, useSelector} from "react-redux";
 import {AppStoreType} from "../h10/bll/store";
 import {changeThemeAC} from "./bll/themeReducer";
 
-const themes = ['dark', 'red', 'some'];
+const themes = ['dark', 'red', 'some', 'orange'];
 
 function HW12() {
     const theme = useSelector<AppStoreType, string>(state => state.theme.theme); // useSelector
