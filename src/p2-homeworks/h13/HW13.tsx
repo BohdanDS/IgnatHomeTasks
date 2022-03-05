576fe0307594e78f9948b10c477b63c9280cd0e3
BohdanDS <bohdanpeliutkevich@gmail.com>
2022-03-05 13:17:52 +0300

    task 13

--- src/p2-homeworks/h13/HW13.tsx
+++ src/p2-homeworks/h13/HW13.tsx
@@ -1,28 +1,10 @@
-import React, {ChangeEvent, useState} from 'react';
-import {Button, Checkbox} from "@mui/material";
-import {NekoCafeAPI} from "./RequestsAPI";
+import React from 'react';
+import Request1 from "./Request1";
 
 const Hw13 = () => {
-    const [checked, setChecked] = useState<boolean>(false)
-    const [response, setResponse] = useState<string>('')
-
-    const handleCheckbox = (e: ChangeEvent<HTMLInputElement>) => {
-        setChecked(!checked)
-    }
-
-    const handleButton = () => {
-        NekoCafeAPI.setCheckbox(checked)
-            .then((response) =>setResponse(response.data.errorText))
-            .catch(error => {
-                setResponse(error.response ? error.response.data.errorText : error.message);
-            })
-    }
-
     return (
         <div>
-            <Checkbox checked={checked} onChange={handleCheckbox}/>
-            <Button variant="outlined" onClick={handleButton}>Submit</Button>
-            {response && <div><h3>{response}</h3><p></p></div>}
+            <Request1/>
         </div>
     );
 };
--- src/p2-homeworks/h5/pages/Junior_Plus.tsx
+++ src/p2-homeworks/h5/pages/Junior_Plus.tsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import HW11 from "../../h11/HW11";
 import HW12 from "../../h12/HW12";
-import HW13 from "../../h13/HW13";
+import HW13 from "../../h13/Request1";
 
 function JuniorPlus() {
     return (
